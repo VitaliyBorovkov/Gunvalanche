1de6bf368b6c8e6221e57b644818ae1e02ff3b3e
Bororvkov Vitalii <theseipartofme@gmail.com>
2025-01-06 22:21:02 +0200

    Add FirstAid
    Added a first aid kit, its selection and restoration of missing health. Fixed bugs, optimised the code a bit.

---
+++ Assets/Scripts/FirstAidKit/MedKit.cs
@@ -0,0 +1,29 @@
+using UnityEngine;
+
+public class MedKit : CollectibleItems
+{
+    [SerializeField] private int healAmount = 50;
+
+    protected override void Collect(GameObject player)
+    {
+        PlayerHealthController playerHealthController = player.GetComponent<PlayerHealthController>();
+
+        if (playerHealthController != null)
+        {
+            if (playerHealthController.GetCurrentHealth() < playerHealthController.GetMaxHealth())
+            {
+                playerHealthController.Heal(healAmount);
+                Debug.Log($"MedKit: {gameObject.name}: Игрок {player.name} исцелён на {healAmount} HP.");
+                base.Collect(player);
+            }
+            else
+            {
+                Debug.Log($"MedKit: {gameObject.name}: У игрока {player.name} полное здоровье и аптечка не была подобрана.");
+            }
+        }
+        else
+        {
+            Debug.Log($"MedKit: {player.name} не имеет PlayerHealthController!", this);
+        }
+    }
+}
