9ac02edb4de57a519310aa4a6293cd9dae7fb65f
TheSei <theseipartofme@gmail.com>
2025-05-08 14:39:59 +0200

    Fix ammo box.
    Fixed spawn ammo boxes.

--- Assets/Scripts/Bullets/BulletsController.cs
+++ Assets/Scripts/Bullets/BulletsController.cs
@@ -25,10 +25,6 @@ public class BulletsController : MonoBehaviour
         enemyLayer = LayerMask.NameToLayer("Enemy");
         environmentLayer = LayerMask.NameToLayer("Environment");
     }
-    //private void Update()
-    //{
-    //    MoveBullet(bulletData);
-    //}
 
     private void OnEnable()
     {
@@ -60,36 +56,10 @@ public class BulletsController : MonoBehaviour
         rb.velocity = direction.normalized * bulletData.Speed;
     }
 
-    //public void SetPool(ObjectPool pool)
-    //{
-    //    objectPool = pool;
-    //    Debug.Log($"BulletsController: {gameObject.name} привязан к пулу {pool.gameObject.name}");
-    //}
-
-    //private void MoveBullet(BulletsData bulletData)
-    //{
-    //    transform.position = Vector3.MoveTowards(transform.position, bulletData.Target, bulletData.Speed * Time.deltaTime);
-    //    if (!bulletData.HitTarget && Vector3.Distance(transform.position, bulletData.Target) < bulletData.DistanceToTarget)
-    //    {
-    //        bulletData.HitTarget = true;
-    //    }
-    //}
-
     private void OnTriggerEnter(Collider other)
     {
-        Debug.Log($"BulletsController: Hit {other.name} on layer {LayerMask.LayerToName(other.gameObject.layer)}");
-
         if (other.gameObject.layer == enemyLayer || other.gameObject.layer == environmentLayer)
         {
-            //if (objectPool != null)
-            //{
-            //    Debug.Log($"BulletsController: {gameObject.name} возвращена в пул {objectPool.gameObject.name}");
-            //    DespawnBullet();
-            //}
-            //else
-            //{
-            //    Debug.LogError($"BulletsController: ObjectPool не назначен для {gameObject.name}!");
-            //}
             HealthController enemyHealth = other.GetComponentInParent<HealthController>();
             if (enemyHealth != null)
             {
@@ -110,15 +80,6 @@ public class BulletsController : MonoBehaviour
 
     private void DespawnBullet()
     {
-        //if (objectPool == null)
-        //{
-        //    Debug.LogError($"BulletsController: ObjectPool не найден при попытке деспавна {gameObject.name}!");
-        //    return;
-        //}
-        //gameObject.SetActive(false);
-        //objectPool.Despawn(gameObject);
-        //Debug.Log($"BulletsController: {gameObject.name} успешно деспавнена в {objectPool.gameObject.name}");
-
         rb.velocity = Vector3.zero;
         gameObject.SetActive(false);
         if (objectPool != null)
